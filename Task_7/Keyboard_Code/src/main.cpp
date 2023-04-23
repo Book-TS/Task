4486f8ca82d1ce6cb804d0b25fb020884f188ab5
BookTS <sachtran1811@gmail.com>
2023-04-24 00:59:20 +0700

    Create Task 8 - 00:59 24/-4/2023

--- Task_7/Keyboard_Code/src/main.cpp
+++ Task_7/Keyboard_Code/src/main.cpp
@@ -3,11 +3,11 @@
 const int pinOut[] = {14, 15, 16, 17, 18};
 const int pinIn[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 19, 20, 21};
 const String KeyCap[5][15] = {
-    {"`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "NULL", "NULL"},
-    {"Tab", "NULL", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "BACK"},
-    {"CAPS", "NULL", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "ENTER", "NULL"},
-    {"NULL", "SHIFT", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "UP", "NULL", "SHIFT", "/"},
-    {"CTRL", "GUI", "NULL", "ALT", "NULL", "NULL", "SPACE", "NULL", "NULL", "NULL", "LEFT", "DOWN", "RIGHT", "NO"}};
+    {"`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="},
+    {"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]"},
+    {"CAPS", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "ENTER"},
+    {"SHIFT", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "UP", "SHIFT", "/"},
+    {"CTRL", "GUI", "ALT", "NULL", "NULL", "NULL", "SPACE", "LEFT", "DOWN", "RIGHT", "CTRL", "NO", "BACK"}};
 
 void scanKey();
 
---
+++ Task_8/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txt;
+    private ImageView imageView;
+//
+//    private DatabaseReference mData;
+
+    private static final int RQ_CODE = 500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mData = FirebaseDatabase.getInstance().getReference();
+
+        txt = findViewById(R.id.txt);
+        imageView = findViewById(R.id.imageView);
+
+//        mData.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                if(snapshot.getValue().toString().equals("1")) txt.setText("Đèn bật");
+//                else txt.setText("Đèn tắt");
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent voice = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                voice.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                voice.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                        Locale.getDefault());
+                voice.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to Control");
+
+                try {
+                    startActivityForResult(voice, RQ_CODE);
+                }
+                catch (Exception e) {
+                    Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RQ_CODE: {
+                if(resultCode == RESULT_OK && data != null) {
+                    ArrayList<String>  result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String a = result.get(0);
+                    if (Objects.equals(a, "bật đèn")) {
+//                        mData.child("control").setValue(1);
+                        txt.setText("Đèn bật");
+                    }
+                    else if (Objects.equals(a, "Tắt Đèn")) {
+//                        mData.child("control").setValue(0);
+                        txt.setText("Đèn tắt");
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
