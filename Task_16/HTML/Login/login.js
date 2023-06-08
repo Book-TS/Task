1a30bcd3d1eb77ae3c701310f621e97f81ca9627
BookTS <sachtran1811@gmail.com>
2023-06-08 22:42:02 +0700

    Update - 22:41 08/06/2023

--- Task_16/HTML/Login/login.js
+++ Task_16/HTML/Login/login.js
@@ -1,21 +1,55 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpPpRkjFtYINVc2QJSimkbCtGIe5zQUrM",
+  authDomain: "dieu-khien-nhiet-do-dixell.firebaseapp.com",
+  databaseURL: "https://dieu-khien-nhiet-do-dixell-default-rtdb.firebaseio.com",
+  projectId: "dieu-khien-nhiet-do-dixell",
+  storageBucket: "dieu-khien-nhiet-do-dixell.appspot.com",
+  messagingSenderId: "961902819526",
+  appId: "1:961902819526:web:cf034f57e9acac8bfa7fc7",
+  measurementId: "G-RWC55H8W3Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+
 // Xử lý sự kiện khi người dùng nhấn nút "Đăng nhập"
 document
   .getElementById("login-form")
   .addEventListener("submit", function (event) {
     event.preventDefault(); // Ngăn chặn gửi biểu mẫu (form) mặc định
 
-    // Lấy giá trị tên đăng nhập và mật khẩu từ các trường input
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    // Kiểm tra thông tin đăng nhập
-    if (username === "admin@gmail.com" && password === "123456") {
-      window.location.href = "../Home/index.html";
-    } else {
-      alert("Tên đăng nhập hoặc mật khẩu không đúng!");
-      // Xóa giá trị trong trường mật khẩu
-      document.getElementById("password").value = "";
-      // Tập trung (focus) vào trường mật khẩu
-      document.getElementById("password").focus();
+    if(email.value == "" || password.value == "") {
+      alert("Vui lòng nhập đầy đủ thông tin");
     }
+
+    var obj = {
+      email: email.value,
+      password: password.value,
+    };
+  
+    console.log(obj);
+  
+    signInWithEmailAndPassword(auth, obj.email, obj.password)
+      .then(function (success) {
+        var aaaa = success.user.uid;
+        localStorage.setItem("uid", aaaa);
+        console.log(aaaa);
+        window.location.replace('../Home/index.html');
+      })
+      .catch(function (err) {
+        alert("Tên tài khoản hoặc mật khẩu không chính xác");
+      });
   });
